54f69940948aba889099982c0558a8975a1920ba
Waldo Heudens <waldo@heudens.be>
2022-10-11 12:26:10 +0200

    Toevoeging van evenhandler

--- Les_5_2/Form1.cs
+++ Les_5_2/Form1.cs
@@ -3,7 +3,12 @@ namespace Les_5_2
     public partial class Form1 : Form
     {
 
+
         public delegate void RemedieDelegate(Student s, TimeSpan teLaat);
+
+
+        // Toegevoegd voor demonstratie event
+        public event RemedieDelegate TeLaat;
 
         public RemedieDelegate mijnDelegates = null;
 
@@ -15,9 +20,18 @@ namespace Les_5_2
 
         }
 
+
+        // Toegevoegd voor demonstratie event
+        public void OnTeLaat(Student s, TimeSpan t)
+        {
+            tbRemedie.Text = "Event werd getriggert: \r\n \r\n";
+            if (TeLaat != null)
+            { TeLaat(s, t); }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            tbRemedie.Text = "";
+            tbRemedie.Text = "De delegate wordt opgeroepen via de knop \n \n";
             if (mijnDelegates != null)
                 mijnDelegates(student1, new TimeSpan(7,0,0,0));
         }
@@ -32,6 +46,7 @@ namespace Les_5_2
             {
                 tbRemedie.Text += "Scheld " + student1.ToString() + " even uit. \r\n";
             }
+
         }
 
         public void Android(Student s, TimeSpan ookTeLaat)
@@ -69,6 +84,32 @@ namespace Les_5_2
                 else
                     mijnDelegates -= new RemedieDelegate(ProjectIntro);
             }
+
+
+            // Toegevoegd voor demonstratie event:  Deze code vervangt vorige als een event wordt gebruikt
+            if (sender == cbAndroid)
+            {
+                if (cbAndroid.Checked)
+                    TeLaat += new RemedieDelegate(Android);
+                else
+                    TeLaat -= new RemedieDelegate(Android);
+            }
+            if (sender == cbDotNet)
+            {
+                if (cbDotNet.Checked)
+                    TeLaat += new RemedieDelegate(DotNetAdvanced);
+                else
+                    TeLaat -= new RemedieDelegate(DotNetAdvanced);
+            }
+            if (sender == cbProjectIntro)
+            {
+                if (cbProjectIntro.Checked)
+                    TeLaat += new RemedieDelegate(ProjectIntro);
+                else
+                    TeLaat -= new RemedieDelegate(ProjectIntro);
+            }
+
+            OnTeLaat(student1, new TimeSpan(8, 0,0, 0));
         }
     }
 }
